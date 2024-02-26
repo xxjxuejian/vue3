5f6a4e2c6ee51af76b7b84999e50bc43f1242e80
xxjxuejian <2498492824@qq.com>
2024-02-26 22:42:48 +0800

    learned ts

---
+++ 备份/21_ts类型接口简单使用/App.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup name="App">
+import Person from "./components/Person.vue";
+import { reactive } from "vue";
+let persons = reactive([
+  { id: 1, name: "张三", age: 18, address: "广州" },
+  { id: 2, name: "李四", age: 20, address: "北京" },
+]);
+</script>
+
+<template>
+  <div class="app">
+    <h1>App父组件</h1>
+    <Person :persons="persons"></Person>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  background-color: #ddd;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
